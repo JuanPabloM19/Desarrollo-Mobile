01e9eb66a53c4a1dc03b9079ccd40bc407b2a805
Juan Pablo <jpmunizaga19@gmail.com>
2023-02-10 11:22:01 -0300

    Primera Entrega

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+import { StatusBar } from 'expo-status-bar';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hola Coder!</Text>
+      <Text>Soy Juan Pablo Munizaga</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
